dc27b132609802b85f3bbec065ccaf7e2d11be66
polar0 <23r0eth@gmail.com>
2022-02-10 14:23:40 +0100

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let word = '';
+  if (num < 0) word = 'ERROR';
+  for (let i = 0; i < num; i++) {
+    word += string;
+  }
+  return word;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...list) {
+  // We got 2 arrays : array, and list including all the optional arguments
+  // Go through all elements of the initial array
+  for (let i = 0; i < array.length; i++) {
+    // Then through each of the optional arguments
+    for (let j = 0; j < list.length; j++) {
+      console.log(array[i], list[j]); // Verify if it's checking everything
+      if (array[i] == list[j]) {
+        let index = array.indexOf(list[i]); // find the index of argument
+        // don't splice if it's not valid
+        if (index !== -1) {
+          array.splice(index, 1);
+        }
+      }
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
